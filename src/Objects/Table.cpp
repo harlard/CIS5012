4c9a9457ea2dd67d06ea2cc6a5a00ec358e34efa
Harlard <marco07091990@gmail.com>
2022-08-09 18:01:39 +0100

    table is a static object

--- src/Objects/Table.cpp
+++ src/Objects/Table.cpp
@@ -1,30 +1,12 @@
 #include "Table.h"
 
-void Table::init(SceneManager* newScnMan, Vector3 newPos, Vector3 newSca){
-  setScnMan(newScnMan);
-  setNewPosition(newPos); // set current position
-  ogreNode->attachObject(objEnt);
-  setNewScale(newSca); // set current scale
-  boundingBoxFromOgre();
-}
+
 void Table::newEntity(SceneManager* newScnMan){
   nameMesh = "Table.mesh";
   objEnt = newScnMan->createEntity(nameMesh);
   }
 
-void Table::createRigidBody(float bodyMass)
-{
-  colShape = new btSphereShape(btScalar(getOgreBound().x/2.0f));
-
-  /// Create Dynamic Objects
-
-  startTransform.setIdentity();
-
-  Quaternion quat2 = ogreNode->_getDerivedOrientation();
-  startTransform.setRotation(btQuaternion(quat2.x, quat2.y, quat2.z, quat2.w));
-
-  Vector3 pos = ogreNode->_getDerivedPosition();
-  startTransform.setOrigin(btVector3(pos.x, pos.y, pos.z));
-  setMass(bodyMass);
-
+void Table::initBullet(float mass, btAlignedObjectArray<btCollisionShape*> &collisionShapes){
+  createRigidBody(mass);
+  addToCollisionShapes(collisionShapes);
 }
--- src/Objects/Table.h
+++ src/Objects/Table.h
@@ -3,19 +3,16 @@
 
 #include "Ogre.h"
 #include "Object4o.h"
-#include "OgreCameraMan.h"
-#include "OgreApplicationContext.h"
+
 
 using namespace Ogre;
-using namespace OgreBites;
 
 class Table : public Object4o{
     private :
 
     public :
-    void init(SceneManager* newScnMan, Vector3 newPos, Vector3 newSca);
-    void createRigidBody(float mass);
     void newEntity(SceneManager* newScnMan);
+    void initBullet(float mass, btAlignedObjectArray<btCollisionShape*> &collisionShapes);
 };
 
 #endif
