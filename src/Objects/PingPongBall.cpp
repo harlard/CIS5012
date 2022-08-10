a70c243ed339b9af65d9c397efa94fb48fbc4532
Harlard <marco07091990@gmail.com>
2022-08-10 11:42:53 +0100

    able to hold the ball and throw but there is something weird with the table collider. I'm aware that in throwB the force variable is not right but I dont have time to fix it

--- src/Objects/PingPongBall.cpp
+++ src/Objects/PingPongBall.cpp
@@ -6,7 +6,13 @@ void PingPongBall::init(SceneManager* newScnMan, Vector3 newPos, Vector3 newSca)
   ogreNode->attachObject(objEnt);
   setNewScale(newSca); // set current scale
   boundingBoxFromOgre();
-}
+  hold = false;
+  iThrowD = Vector3(1.0,0.0,0.0);
+  cThrowD = iThrowD;
+  iThrowS = 10000.0;
+  cThrowS = iThrowS;
+
+  }
 void PingPongBall::newEntity(SceneManager* newScnMan){
   nameMesh = "PingPongBall.mesh";
   objEnt = newScnMan->createEntity(nameMesh);
@@ -27,4 +33,58 @@ void PingPongBall::createRigidBody(float bodyMass)
   startTransform.setOrigin(btVector3(pos.x, pos.y, pos.z));
   setMass(bodyMass);
 
+}
+
+void PingPongBall::throwB(){
+  hold = false;
+  btVector3 force(-(cThrowD.x)*cThrowS, (cThrowD.y)*cThrowS, (cThrowD.z)*cThrowS);
+  btVector3 push;
+
+  btTransform trans;
+
+  if (body && body->getMotionState())
+  {
+      //get the orientation of the rigid body in world space.
+      body->getMotionState()->getWorldTransform(trans);
+      btQuaternion orientation = trans.getRotation();
+
+      //rotate the local force, into the global space.
+      //i.e. push in down the local z.
+      push = quatRotate(orientation, force);
+
+      //activate the body, this is essential if the body
+      //has gone to sleep (i.e. stopped moving/colliding).
+      body->activate();
+
+      //apply a force to the center of the body
+      body->applyCentralForce(push);
+  }
+}
+
+void PingPongBall::recover(){
+  hold = true;
+  ogreNode->setPosition(throwP);
+  reposition(throwP);
+  body->setActivationState(0);
+  cThrowD = iThrowD;
+  cThrowS = iThrowS;
+}
+
+void PingPongBall::setTP(Vector3 newTP){throwP = newTP;}
+void PingPongBall::setTS(float newTS){iThrowS = newTS; cThrowS = iThrowS;}
+void PingPongBall::setTD(Vector3 newTD){iThrowD = newTD; cThrowD = iThrowD;}
+bool PingPongBall::getHold(){return hold;}
+
+void PingPongBall::update()
+{
+  if(!hold){
+   btTransform trans;
+  if (body && body->getMotionState())
+  {
+    body->getMotionState()->getWorldTransform(trans);
+    btQuaternion orientation = trans.getRotation();
+
+    ogreNode->setPosition(Ogre::Vector3(trans.getOrigin().getX(), trans.getOrigin().getY(), trans.getOrigin().getZ()));
+    ogreNode->setOrientation(Ogre::Quaternion(orientation.getW(), orientation.getX(), orientation.getY(), orientation.getZ()));
+  }}
 }
--- src/Objects/PingPongBall.h
+++ src/Objects/PingPongBall.h
@@ -11,11 +11,25 @@ using namespace OgreBites;
 
 class PingPongBall : public Object4o{
     private :
+      bool hold;
+      float iThrowS;
+      float cThrowS;
+      Vector3 iThrowD;
+      Vector3 cThrowD;
+      Vector3 throwP;
 
     public :
     void init(SceneManager* newScnMan, Vector3 newPos, Vector3 newSca);
     void createRigidBody(float mass);
     void newEntity(SceneManager* newScnMan);
+    void throwB();
+    void recover();
+    void setTP(Vector3 newTP);
+    void setTS(float newTS);
+    void setTD(Vector3 newTD);
+    void update();
+    bool getHold();
+
 };
 
 #endif
